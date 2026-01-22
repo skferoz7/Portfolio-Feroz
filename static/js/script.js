99d0cf6ee0b8c7a069b43f4e97ee6748e039833d
shaik feroz <ferozzz0655@gmail.com>
2026-01-22 12:46:56 +0530

    change footer logic

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.getElementById("themeToggle");
-  const link = document.getElementsByClassName("link");
+  const links = document.getElementsByClassName("link");
+  const copies = document.getElementsByClassName("copy");
 
   if (!toggleBtn) return;
 
@@ -8,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function () {
   if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark-mode");
     toggleBtn.textContent = "☀️";
+    for (let l of links) l.style.color = "white";
+    for (let c of copies) c.style.color = "white";
   }
 
   toggleBtn.addEventListener("click", function () {
@@ -16,10 +19,15 @@ document.addEventListener("DOMContentLoaded", function () {
     if (document.body.classList.contains("dark-mode")) {
       localStorage.setItem("theme", "dark");
       toggleBtn.textContent = "☀️";
-      link.style.color = "white";
-    } else {
+       for (let l of links) l.style.color = "white";
+      for (let c of copies) c.style.color = "white";
+    }
+    else {
       localStorage.setItem("theme", "light");
       toggleBtn.textContent = "🌙";
+       for (let l of links) l.style.color = "white";
+      for (let c of copies) c.style.color = "white";
+  
     }
   });
 });
